45ef7ff601cc5c6fc5ee5d6ea1965bb673371212
RoyChan1995 <roychan19990119@gmail.com>
2023-12-29 18:03:05 +0100

    feat:VIPSignInBonusStatistic

--- src/views/vip-management/singin-bonus/statistic.vue
+++ src/views/vip-management/singin-bonus/statistic.vue
@@ -6,6 +6,26 @@ import moment from 'moment-timezone';
 import { fa } from 'element-plus/es/locale';
 import { NumberFormatProps } from 'vue-i18n';
 import { ArrowLeft, CopyDocument, ArrowRight, ArrowDown } from '@element-plus/icons-vue';
+
+interface VIPSignInBonusRankStatistic{
+    vip_rank:string,
+    vip_rank_account_count:number,
+    vip_signin_account_count:number,
+    vip_checkin_count:number,
+    vip_claim_reward_amount:number,
+}
+
+interface VIPSignInBonusLevelStatistic{
+    vip_level:string,
+    vip_level_account_count:number,
+    vip_signin_account_count:number,
+    vip_checkin_count:number,
+    vip_claim_reward_amount:number,
+}
+
+const vip_signin_bonus_rank_statistic_list = ref<Array<VIPSignInBonusRankStatistic>>()
+const vip_signin_bonus_level_statistic_list = ref<Array<VIPSignInBonusLevelStatistic>>()
+
 const router = useRouter();
 const goBack = () => {
     router.push({ name: "VIPSigninBonus"});
@@ -22,14 +42,19 @@ const formDataRank = ref({
     claim_bonus_account_op:'≥',
     claim_bonus_account_number:''
 })
-
 const formDataLevel = ref({
-    level_op:'≥',
-    level_number:'',
-    vip_rank:'',
-    level_clients_op:'≥',
-    level_clients_number:'',
+    vip_rank_op:'≥',
+    vip_rank_number:'',
+    vip_rank_count_op:'≥',
+    vip_rank_count_number:'',
+    vip_singin_count_op:'≥',
+    vip_singin_count_number:'',
+    check_ins_count_op:'≥',
+    check_ins_count_number:'',
+    claim_bonus_account_op:'≥',
+    claim_bonus_account_number:''
 })
+
 const buttonIndex = ref<number>(0);
 const dateRange = ref<Array<any>>([
         moment.tz("Asia/Hong_Kong").format("YYYY-MM-DD"),
@@ -173,8 +198,8 @@ const vip_ranks = ref<Array<string>>(['Iron', 'Bronze', 'Silver', 'Gold', 'Plati
                         <el-input v-model="formDataRank.vip_rank_count_number" placeholder="请输入人数" style="width:200px"/>
                     </el-form-item>
 
-                    <el-form-item label="VIP段位账号数" style="display:flex">
-                        <el-select v-model="formDataRank.vip_rank_count_op" style="width: 80px;">
+                    <el-form-item label="签到账号数量" style="display:flex">
+                        <el-select v-model="formDataRank.vip_singin_count_op" style="width: 80px;">
                             <el-option
                                 v-for="item in op_options"
                                 :key="item.value"
@@ -182,11 +207,112 @@ const vip_ranks = ref<Array<string>>(['Iron', 'Bronze', 'Silver', 'Gold', 'Plati
                                 :value="item.value">
                             </el-option>
                         </el-select>
-                        <el-input v-model="formDataRank.vip_rank_count_number" placeholder="请输入人数" style="width:200px"/>
+                        <el-input v-model="formDataRank.vip_singin_count_number" placeholder="请输入人数" style="width:200px"/>
                     </el-form-item>
                 </el-form>
+                <el-form class="left_position" style="display:flex;justify-content:space-between; padding-right:20px;">
+                    <div style="display:flex">
+                        <el-form-item label="签到次数" style="display:flex">
+                            <el-select v-model="formDataRank.check_ins_count_op" style="width: 80px;">
+                                <el-option
+                                    v-for="item in op_options"
+                                    :key="item.value"
+                                    :label="item.label"
+                                    :value="item.value">
+                                </el-option>
+                            </el-select>
+                            <el-input v-model="formDataRank.check_ins_count_number" placeholder="请输入次数" style="width:200px"/>
+                        </el-form-item>
+
+                        <el-form-item label="领取奖金金额" style="display:flex">
+                            <el-select v-model="formDataRank.claim_bonus_account_op" style="width: 80px;">
+                                <el-option
+                                    v-for="item in op_options"
+                                    :key="item.value"
+                                    :label="item.label"
+                                    :value="item.value">
+                                </el-option>
+                            </el-select>
+                            <el-input v-model="formDataRank.claim_bonus_account_number" placeholder="请输入金数" style="width:200px"/>
+                        </el-form-item>
+                    </div>
+                    <div>
+                        <el-button  :icon="Refresh">重置</el-button>
+                        <el-button  :icon="Search">查新</el-button>
+                    </div>
+                </el-form>
             </div>
-            
+
+            <div v-if="tab_index == 1" class="search">
+                <el-form class="left_position" style="display:flex">
+                    <el-form-item label="VIP段位账号数" style="display:flex">
+                        <el-select v-model="formDataLevel.vip_rank_op" style="width: 80px;">
+                            <el-option
+                                v-for="item in op_options"
+                                :key="item.value"
+                                :label="item.label"
+                                :value="item.value">
+                            </el-option>
+                        </el-select>
+                        <el-input v-model="formDataLevel.vip_rank_number" placeholder="请输入人数" style="width:200px"/>
+                    </el-form-item>
+                    
+                    <el-form-item label="VIP段位账号数" style="display:flex">
+                        <el-select v-model="formDataLevel.vip_rank_count_op" style="width: 80px;">
+                            <el-option
+                                v-for="item in op_options"
+                                :key="item.value"
+                                :label="item.label"
+                                :value="item.value">
+                            </el-option>
+                        </el-select>
+                        <el-input v-model="formDataLevel.vip_rank_count_number" placeholder="请输入人数" style="width:200px"/>
+                    </el-form-item>
+
+                    <el-form-item label="签到账号数量" style="display:flex">
+                        <el-select v-model="formDataLevel.vip_singin_count_op" style="width: 80px;">
+                            <el-option
+                                v-for="item in op_options"
+                                :key="item.value"
+                                :label="item.label"
+                                :value="item.value">
+                            </el-option>
+                        </el-select>
+                        <el-input v-model="formDataLevel.vip_singin_count_number" placeholder="请输入人数" style="width:200px"/>
+                    </el-form-item>
+                </el-form>
+                <el-form class="left_position" style="display:flex;justify-content:space-between; padding-right:20px;">
+                    <div style="display:flex">
+                        <el-form-item label="签到次数" style="display:flex">
+                            <el-select v-model="formDataLevel.check_ins_count_op" style="width: 80px;">
+                                <el-option
+                                    v-for="item in op_options"
+                                    :key="item.value"
+                                    :label="item.label"
+                                    :value="item.value">
+                                </el-option>
+                            </el-select>
+                            <el-input v-model="formDataLevel.check_ins_count_number" placeholder="请输入次数" style="width:200px"/>
+                        </el-form-item>
+
+                        <el-form-item label="领取奖金金额" style="display:flex">
+                            <el-select v-model="formDataLevel.claim_bonus_account_op" style="width: 80px;">
+                                <el-option
+                                    v-for="item in op_options"
+                                    :key="item.value"
+                                    :label="item.label"
+                                    :value="item.value">
+                                </el-option>
+                            </el-select>
+                            <el-input v-model="formDataLevel.claim_bonus_account_number" placeholder="请输入金数" style="width:200px"/>
+                        </el-form-item>
+                    </div>
+                    <div>
+                        <el-button  :icon="Refresh">重置</el-button>
+                        <el-button  :icon="Search">查新</el-button>
+                    </div>
+                </el-form>
+            </div>
             <el-card style="margin-top: 10px;">
                 <el-row style="justify-content: space-between;">
                     <div>              
@@ -202,10 +328,6 @@ const vip_ranks = ref<Array<string>>(['Iron', 'Bronze', 'Silver', 'Gold', 'Plati
                         <el-button :type="buttonIndex == 8 ? 'primary' : ''" @click="handledateRange('last year')">去年</el-button>
                         <el-button :type="buttonIndex == 9 ? 'primary' : ''" @click="handledateRange('last 7days')">过去7天</el-button>
                         <el-button :type="buttonIndex == 10 ? 'primary' : ''" @click="handledateRange('last 30days')">过去30天</el-button>
-                    </div>
-                    <div>
-                        <el-button  :icon="Refresh">重置</el-button>
-                        <el-button  :icon="Search">查新</el-button>
                     </div>
                 </el-row>
                 <el-row style="margin-top: 20px;">
@@ -227,9 +349,45 @@ const vip_ranks = ref<Array<string>>(['Iron', 'Bronze', 'Silver', 'Gold', 'Plati
                 </el-row>
             </el-card>
         </el-col>
-        <div style="margin-top:20px">
-            <el-button :type="tab_index == 0 ? 'warning' : ''" @click="select_tab(0)">段位人数分布</el-button>
-            <el-button :type="tab_index == 1 ? 'warning' : ''" @click="select_tab(1)">等级人数分布</el-button>
+        <div style="margin-top:20px; display:flex; justify-content:space-between">
+            <div>
+                <el-button :type="tab_index == 0 ? 'warning' : ''" @click="select_tab(0)">段位</el-button>
+                <el-button :type="tab_index == 1 ? 'warning' : ''" @click="select_tab(1)">等级</el-button>
+            </div>
+            <div style="display: flex;">
+                <div class="data-pane">签到账号数量: 9999999</div>
+                <div class="data-pane">签到次数: 9999999</div>
+                <div class="data-pane">领取奖励金额: 9999999</div>
+            </div>
+        </div>
+        <div>              
+            <el-card :span="24" :xs="24">
+                <el-table v-if="tab_index == 0" :data="vip_signin_bonus_rank_statistic_list" style="width: 100%;">
+                    <el-table-column label = "VIP段位" align="left" prop="vip_rank">
+                    </el-table-column>
+                    <el-table-column label = "段位内总账号数量" sortable  align="left" prop="vip_rank_account_count">
+                    </el-table-column>
+                    <el-table-column label="签到账号数量" sortable  align="left" prop="vip_signin_account_count">
+                    </el-table-column>
+                    <el-table-column label="签到次数" sortable  align="left" prop="vip_checkin_count">
+                    </el-table-column>
+                    <el-table-column label="领取奖励金额" sortable  align="left" prop="vip_claim_reward_amount">
+                    </el-table-column>
+                </el-table>
+
+                <el-table v-if="tab_index == 1" :data="vip_signin_bonus_level_statistic_list" style="width: 100%;">
+                    <el-table-column label = "VIP等级"  align="left" prop="vip_level">
+                    </el-table-column>
+                    <el-table-column label = "等级内总账号数量" sortable  align="left" prop="vip_level_account_count">
+                    </el-table-column>
+                    <el-table-column label="签到账号数量"  sortable  align="left" prop="vip_signin_account_count">
+                    </el-table-column>
+                    <el-table-column label="签到次数" sortable  align="left" prop="vip_checkin_count">
+                    </el-table-column>
+                    <el-table-column label="领取奖励金额" sortable  align="left" prop="vip_claim_reward_amount">
+                    </el-table-column>
+                </el-table>
+            </el-card>
         </div>
     </div>
 
@@ -239,5 +397,13 @@ const vip_ranks = ref<Array<string>>(['Iron', 'Bronze', 'Silver', 'Gold', 'Plati
 .el-table thead th.el-table__cell {
     background: #f5f7fa !important;
     height: 100px;
+}
+
+.data-pane{
+    border: 1px solid #666;
+    border-radius: 5px;
+    padding: 5px 10px;
+    margin:0px 5px;
+    color: #666;
 }
 </style>
