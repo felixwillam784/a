0c4600bc3bde76806e0def216efd1c887502bb82
RoyChan1995 <roychan19990119@gmail.com>
2024-01-17 18:04:37 +0100

    feat:AddBonus

---
+++ src/views/bonus-management/main/components/usage-scope/GameType.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+interface GameType {
+  type_name: string;
+  games: Array<string>;
+}
+
+const game_type_list = ref<Array<GameType>>([
+  {
+    type_name: "Casino",
+    games: ["Originals", "Slots", "Live Casino", "Table Games"],
+  },
+  {
+    type_name: "Sports",
+    games: [
+      "Live Events",
+      "Soccer",
+      "Tennis",
+      "Basket",
+      "Cricket",
+      "FIFA",
+      "American Football",
+      "Ice Hockey",
+      "Baseball",
+      "Handball",
+    ],
+  },
+]);
+</script>
+<template>
+  <el-collapse>
+    <el-collapse-item v-for="(item, index) in game_type_list" :key="index">
+      <template #title>
+        <el-checkbox style="margin-right: 10px"> </el-checkbox>
+        {{ item.type_name }}
+      </template>
+      <template #default>
+        <div style="display: flex; flex-wrap: wrap; gap: 15px; padding-left: 30px">
+          <div
+            style="display: flex; align-items: center; gap: 5px"
+            v-for="(it, ind) in item.games"
+            :key="ind"
+          >
+            <el-checkbox></el-checkbox>
+            <p>{{ it }}</p>
+          </div>
+        </div>
+      </template>
+    </el-collapse-item>
+  </el-collapse>
+</template>
+<style lang="scss"></style>
--- src/views/bonus-management/main/index.vue
+++ src/views/bonus-management/main/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import AddDialog from "./components/add.vue";
+
 interface GetBonusData {
   bonus_id: number;
   bonus_type: string;
@@ -47,6 +49,30 @@ const bonus_data_list = ref<Array<GetBonusData>>([
     bonus_usage_scope: ["游戏分类"],
     bonus_code_condition: "无限制",
   },
+  {
+    bonus_id: 1004,
+    bonus_type: "会员日奖金",
+    bonus_name: "会员日奖金",
+    bonus_descript: "会员日奖金",
+    bonus_amount: "50",
+    bonus_cap: 800,
+    bonus_code_magnification: 20,
+    bonus_code_failure_condition: 5,
+    bonus_code_time_limit: 30,
+    bonus_expiration_time: "2023-12-30 00:00:00",
+    bonus_VIP_restriction: "",
+    bonus_usage_scope: [
+      "游戏分类",
+      "游戏渠道",
+      "游戏ID",
+      "游戏ID",
+      "游戏ID",
+      "游戏ID",
+      "游戏ID",
+      "游戏ID",
+    ],
+    bonus_code_condition: "无限制",
+  },
 ]);
 
 const get_class_name_for_usage_scope = (param: string) => {
@@ -55,7 +81,22 @@ const get_class_name_for_usage_scope = (param: string) => {
   if (param == "游戏ID") return "green-background";
   return "";
 };
+
+const filter_usage_scope = (data: Array<string>, standard: string) => {
+  let res: string[] = [];
+  data.forEach((element) => {
+    if (element == standard) res.push(element);
+  });
+  return res;
+};
+
+const AddDialogRef = ref();
+
+const add_new = () => {
+  AddDialogRef.value.show_dialog(true);
+};
 </script>
+
 <template>
   <div class="app-container">
     <div class="search">
@@ -90,7 +131,7 @@ const get_class_name_for_usage_scope = (param: string) => {
             </el-form-item>
           </div>
           <div>
-            <el-button>新增奖金</el-button>
+            <el-button @click="add_new">新增奖金</el-button>
             <el-button>Excel导入</el-button>
             <el-button>Excel导出</el-button>
           </div>
@@ -198,14 +239,14 @@ const get_class_name_for_usage_scope = (param: string) => {
 
         <el-table-column
           label="奖金使用范围"
-          width="200px"
+          width="350px"
           align="center"
           prop="bonus_usage_scope"
         >
           <template #default="scope">
-            <div style="display: flex; justify-content: center; flex-wrap: wrap">
+            <div style="display: flex; flex-wrap: wrap">
               <div
-                v-for="(item, index) in scope.row.bonus_usage_scope"
+                v-for="(item, index) in scope.row.bonus_usage_scope.slice(0, 4)"
                 :key="index"
                 :class="get_class_name_for_usage_scope(item)"
               >
@@ -215,7 +256,63 @@ const get_class_name_for_usage_scope = (param: string) => {
                 v-if="scope.row.bonus_usage_scope[0] != '无限制'"
                 style="display: flex; justify-content: center; align-items: center"
               >
-                <el-button>...</el-button>
+                <el-tooltip effect="light">
+                  <template #default>
+                    <el-button> ... </el-button>
+                  </template>
+                  <template #content>
+                    <div style="padding: 5px; width: 400px">
+                      <div class="dl_row">游戏分类</div>
+                      <div
+                        class="dl_row"
+                        style="display: flex; justify-content: start; flex-wrap: wrap"
+                      >
+                        <div
+                          v-for="(item, index) in filter_usage_scope(
+                            scope.row.bonus_usage_scope,
+                            '游戏分类'
+                          )"
+                          :key="index"
+                          :class="get_class_name_for_usage_scope(item)"
+                        >
+                          {{ item }}
+                        </div>
+                      </div>
+                      <div class="dl_row">游戏渠道</div>
+                      <div
+                        class="dl_row"
+                        style="display: flex; justify-content: start; flex-wrap: wrap"
+                      >
+                        <div
+                          v-for="(item, index) in filter_usage_scope(
+                            scope.row.bonus_usage_scope,
+                            '游戏渠道'
+                          )"
+                          :key="index"
+                          :class="get_class_name_for_usage_scope(item)"
+                        >
+                          {{ item }}
+                        </div>
+                      </div>
+                      <div class="dl_row">游戏</div>
+                      <div
+                        class="dl_row"
+                        style="display: flex; justify-content: start; flex-wrap: wrap"
+                      >
+                        <div
+                          v-for="(item, index) in filter_usage_scope(
+                            scope.row.bonus_usage_scope,
+                            '游戏ID'
+                          )"
+                          :key="index"
+                          :class="get_class_name_for_usage_scope(item)"
+                        >
+                          {{ item }}
+                        </div>
+                      </div>
+                    </div>
+                  </template>
+                </el-tooltip>
               </div>
             </div>
           </template>
@@ -246,8 +343,11 @@ const get_class_name_for_usage_scope = (param: string) => {
         </el-table-column>
       </el-table>
     </el-card>
+
+    <AddDialog ref="AddDialogRef" />
   </div>
 </template>
+
 <style lang="scss">
 .yellow-background {
   background-color: yellow;
@@ -263,5 +363,22 @@ const get_class_name_for_usage_scope = (param: string) => {
   background-color: green;
   padding: 5px;
   margin: 5px;
+}
+
+.dl_row {
+  padding: 1px 20px;
+  margin-bottom: 4px;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+}
+.dl_row:nth-child(2n + 1) {
+  /* Style for odd elements in the group */
+  background-color: #aaa;
+}
+
+.dl_row:nth-child(2n) {
+  /* Style for even elements in the group */
+  background-color: #ccc;
 }
 </style>
